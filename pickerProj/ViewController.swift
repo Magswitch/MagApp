49a911895eab4a8901ec528e15e2d0c61e9a151f
a_dog <a_dog@a-dogs-MacBook-Pro.local>
2015-06-08 19:29:55 -0600

    Initial Commit

---
+++ pickerProj/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  pickerProj
+//
+//  Created by a_dog on 6/8/15.
+//  Copyright (c) 2015 a_dog. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
