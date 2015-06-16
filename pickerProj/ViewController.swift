a062c2c2590f936c49d00bef250a482fc5d16ec4
Andrew Gentry <andrew.gentry@colorado.edu>
2015-06-15 23:18:40 -0600

    Made the transitions work even better
    also structured the code a little bit and refined more.

--- pickerProj/ViewController.swift
+++ pickerProj/ViewController.swift
@@ -12,71 +12,86 @@ import CoreBluetooth
 class ViewController: UIViewController, UIPickerViewDelegate, UIPickerViewDataSource, CBCentralManagerDelegate, CBPeripheralDelegate{
     
     
-                    ///////////////
-    ///////////////// Variables  ////////////////////////////////
+//////////////////////////
+    //
+    //    Variables
+    //
+//////////////////////////
     
     
+    
+    //Views
     let connectButtonView = UIView()
-    let safteyButtonView = UIView()
+    let safetyStatusView = UIView()
     
+    //Colors
     var backgroundTransparencyDown: UIColor = UIColor(red: 0.25, green: 0.0, blue: 0.5, alpha: 0.05)
     var backgroundTransparencyUp: UIColor = UIColor(red: 0.25, green: 0.3, blue: 0.2, alpha: 0.0)
     var blueTransparency: UIColor = UIColor(red: 0.0, green: 0.0, blue: 1.0, alpha: 0.15)
-    var greenTransparency: UIColor = UIColor(red: 0.0, green: 1.0, blue: 0.0, alpha: 0.2)
-    var redTransparency: UIColor = UIColor(red: 1.0, green: 0.0, blue: 0.0, alpha: 0.2)
-  
-    let toolArray = ["AR15", "AR20", "AR30","PLAY 20x4","PLAY 50x2","PLAY 50x3","PLAY 50x4", "PLAY 70x2", "PLAY 70x3","PLAY70x4","E-Drive 50mm", "E-Drive 30mm", "SF600"]
-    let valueArray = ["24", "23","22","21","20","19","18","17","16","15","14","13","12","11","10","9","8","7","6","5","4","3","2","1","7/8","3/4","1/2","1/4","1/8","1/10"]
+    var greenTransparency: UIColor = UIColor(red: 0.0, green: 1.0, blue: 0.0, alpha: 0.3)
+    var redTransparency: UIColor = UIColor(red: 1.0, green: 0.0, blue: 0.0, alpha: 0.4)
+    var amberTransparency: UIColor = UIColor(red: 1.0, green: 0.1, blue: 0.0, alpha: 0.3)
+    
+    //Arrays
+    let toolArray = ["PLAY20x4","PLAY20x6","PLAY40x2","E-Drive 50mm", "E-Drive 30mm", "SF600"]
+    let valueArray = ["24", "22", "20","17","12","11","10","5","4","3","2","1","3/4","1/2","1/4","1/8","1/10"]
     let unitArray = ["Gauge", "cm", "mm", "in"]
     
+    //Bluetooth
     var connectionStatus: String = ""
-    
     var centralManager : CBCentralManager!
     var sensorTagPeripheral : CBPeripheral!
     var discoveredPeripheral:CBPeripheral?
     
     
     
+////////////////////////
+    //
+    //   Outlets
+    //
+///////////////////////
     
+
     
-    
-    
-                     ///////////
-    ///////////////// Outlets //////////////////////////////////////
-    
-    @IBOutlet weak var littleLabel: UILabel!
+    @IBOutlet weak var unsafeLabel: UILabel!
+    @IBOutlet weak var cautionLabel: UILabel!
+    @IBOutlet weak var safeLabel: UILabel!
     @IBOutlet weak var backgroundView: UIView!
     @IBOutlet weak var scanButton: UIButton!
     @IBOutlet weak var connectionLabel: UILabel!
     @IBOutlet weak var connectedLabel: UILabel!
     
-    @IBAction func thirdButton(sender: AnyObject) {
+    
+    @IBAction func Unsafe(sender: AnyObject) {
         
+        safetyBarPositionSwap("Unsafe")
         
     }
-    
-    func updateConnectionStatus(){
-        
-        connectionLabel.text = connectionStatus
-    }
-    
-   
+
     
     @IBAction func refreshButtonRelease(sender: AnyObject) {
         
-        littleLabel.text = "Unsafe"
+       
         
         updateConnectionStatus()
         UIView.animateWithDuration(0.3, animations: {self.connectButtonView.backgroundColor = self.blueTransparency; self.connectButtonView.frame = CGRect(x: 0, y: 380, width: 190, height: 50)})
         
         UIView.transitionWithView(connectionLabel, duration: 1.2, options: UIViewAnimationOptions.TransitionCrossDissolve, animations: { self.connectionLabel.textColor = UIColor.blackColor()}, completion: nil)
-         UIView.transitionWithView(connectedLabel, duration: 0.2, options: UIViewAnimationOptions.TransitionCrossDissolve, animations: { self.connectedLabel.textColor = self.backgroundTransparencyUp}, completion: nil)
+        UIView.transitionWithView(connectedLabel, duration: 0.2, options: UIViewAnimationOptions.TransitionCrossDissolve, animations: { self.connectedLabel.textColor = self.backgroundTransparencyUp}, completion: nil)
+        
+        self.view.bringSubviewToFront(self.connectionLabel)
+        
+        safetyStatus = "Caution"
+        
+        
+        safetyBarPositionSwap(getSafetyBarStatus())
+        
         
     }
     
     
     @IBAction func scanButton(sender: AnyObject) {
-        littleLabel.text = "Safe "
+      
         centralManager.scanForPeripheralsWithServices(nil, options: nil)
         connectionStatus = "Scanning..."
         connectedLabel.text = "Okay"
@@ -88,13 +103,90 @@ class ViewController: UIViewController, UIPickerViewDelegate, UIPickerViewDataSo
         UIView.transitionWithView(connectionLabel, duration: 0.2, options: UIViewAnimationOptions.TransitionCrossDissolve, animations: { self.connectionLabel.textColor = self.backgroundTransparencyUp}, completion: nil)
         UIView.transitionWithView(connectedLabel, duration: 1.2, options: UIViewAnimationOptions.TransitionCrossDissolve, animations: { self.connectedLabel.textColor = UIColor.blackColor()}, completion: nil)
         
+        safetyStatus = "Safe"
+        
+        
+        safetyBarPositionSwap(getSafetyBarStatus())
+        
+        
+    }
+    
+//////////////////////////
+    //
+    //    Transistions
+    //
+//////////////////////////
+    
+    var safetyStatus:String = ""
+    
+
+    func getSafetyBarStatus()->String {
+        
+        return safetyStatus
+        
+    }
+    
+    func fadeOutLabel(label: UILabel, duration:NSTimeInterval) {
+        
+        UIView.transitionWithView(label, duration: duration, options: UIViewAnimationOptions.TransitionCrossDissolve, animations: {label.textColor = self.backgroundTransparencyUp}, completion: nil)
+        
+        
+    }
+    
+    func fadeInLabel(label: UILabel, duration:NSTimeInterval) {
+        
+       UIView.transitionWithView(label, duration: duration, options: UIViewAnimationOptions.TransitionCrossDissolve, animations: {label.textColor = UIColor.blackColor()}, completion: nil)
+        
     }
     
     
+    func safetyBarPositionSwap(safetyStatus:String) {
+        
+        switch safetyStatus {
+            
+            case "Unsafe":
+                
+            UIView.animateWithDuration(0.3, animations: {self.safetyStatusView.backgroundColor = self.redTransparency; self.safetyStatusView.frame = CGRect(x: 0, y: 430, width: 190, height: 50)})
+            fadeInLabel(unsafeLabel, duration: 1.2)
+            fadeOutLabel(cautionLabel, duration: 0.2)
+            fadeOutLabel(safeLabel, duration: 0.2)
+            
+            
+            
+            
+            case "Caution":
+            
+            UIView.animateWithDuration(0.3, animations: {self.safetyStatusView.backgroundColor = self.amberTransparency; self.safetyStatusView.frame = CGRect(x: 0+95, y: 430, width: 190, height: 50)})
+            fadeInLabel(cautionLabel, duration: 1.2)
+            fadeOutLabel(unsafeLabel, duration: 0.2)
+            fadeOutLabel(safeLabel, duration: 0.2)
+            
+            
+            case "Safe":
+            
+            UIView.animateWithDuration(0.3, animations: {self.safetyStatusView.backgroundColor = self.greenTransparency; self.safetyStatusView.frame = CGRect(x: 0+190, y: 430, width: 190, height: 50)})
+            fadeInLabel(safeLabel, duration: 1.2)
+            fadeOutLabel(unsafeLabel, duration: 0.2)
+            fadeOutLabel(cautionLabel, duration: 0.2)
+            
+            default:
+            
+                return
+            
+        }
     
-                   ///////////////////////
-    /////////////// Bluetooth Management ////////////////////////////////////
+    }
+
+/////////////////////
+    //
+    //   Bluetooth
+    //
+/////////////////////
     
+    func updateConnectionStatus(){
+        
+        connectionLabel.text = connectionStatus
+    }
     
     
     func centralManagerDidUpdateState(central: CBCentralManager!) {
@@ -110,7 +202,7 @@ class ViewController: UIViewController, UIPickerViewDelegate, UIPickerViewDataSo
             println("ON")
             connectionStatus = "Connected"
             updateConnectionStatus()
-           
+            
             self.scanButton(self)
             break
         case .Resetting:
@@ -144,17 +236,18 @@ class ViewController: UIViewController, UIPickerViewDelegate, UIPickerViewDataSo
         
         var discoveredPeripheral: CBPeripheral
         
-        
     }
     
-                  ////////////////////////////
-    /////////////// PickerView Functionality /////////////////////////////////
-    
+///////////////////////
+    //
+    //   Picker View
+    //
+///////////////////////
     
     func numberOfComponentsInPickerView(pickerView: UIPickerView) -> Int {
         return 3
     }
-
+    
     
     func pickerView(pickerView: UIPickerView, widthForComponent component: Int) -> CGFloat {
         
@@ -174,8 +267,6 @@ class ViewController: UIViewController, UIPickerViewDelegate, UIPickerViewDataSo
             
             
         }
-        
-        
     }
     
     func pickerView(pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
@@ -210,37 +301,42 @@ class ViewController: UIViewController, UIPickerViewDelegate, UIPickerViewDataSo
             
         default:
             return valueArray[row]
-        
+            
         }
     }
-
+    
     
     override func viewDidLoad() {
+        
         super.viewDidLoad()
-    
+        
+        updateConnectionStatus()
+        
         connectButtonView.backgroundColor = self.blueTransparency
         connectButtonView.frame = CGRect(x: 0, y: 380, width: 190, height: 50)
         
-        safteyButtonView.backgroundColor = self.redTransparency
-        safteyButtonView.frame = CGRect(x: 0, y: 430, width: 190, height: 50)
+        safetyStatusView.backgroundColor = self.redTransparency
+        safetyStatusView.frame = CGRect(x: 0, y: 430, width: 190, height: 50)
         
-        littleLabel.text = connectionStatus
+        cautionLabel.textColor = self.backgroundTransparencyUp
+        safeLabel.textColor = self.backgroundTransparencyUp
+        
+       
         self.connectedLabel.text = ""
         
+        self.view.addSubview(safetyStatusView)
         self.view.addSubview(connectButtonView)
-        self.view.addSubview(safteyButtonView)
         
         
         centralManager = CBCentralManager(delegate: self, queue: nil)
-      
+        
         // Do any additional setup after loading the view, typically from a nib.
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    
+    
 }
-
